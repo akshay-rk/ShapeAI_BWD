f426024c71b23b46c13318f920fb956231bbc66c
akshay-rk <72289053+akshay-rk@users.noreply.github.com>
2021-06-07 11:37:13 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "../public/styles.css";
+import React from "react";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Java Script and React.js</h1>
+      <h2>This was an amazing bootcamp taken up by Shaurya Sinha
+        we have covered everything from scratch including Javascript, React.js, HTML.
+      </h2>
+    </div>
+  );
+}
